e050af153d20e559797dffdeaf18978b2cea9c2a
Hieu-Thi Luong <luonghieuthi@gmail.com>
2024-03-13 00:37:47 +0800

    Remove unnecessary lines

--- assets/js/lib/abcjs.js
+++ assets/js/lib/abcjs.js
@@ -1,10 +1,6 @@
 import ABCJS from 'abcjs'
 
 const abcjsElements = document.getElementsByClassName('abcjs')
-if (abcjsElements.length) {
-  console.log(ABCJS);
-
-  Array.from(abcjsElements).forEach((e) => {
-    ABCJS.renderAbc(e.id, e.textContent, { responsive: "resize" });
-  })
-}
+Array.from(abcjsElements).forEach((e) => {
+  ABCJS.renderAbc(e.id, e.textContent, { responsive: "resize" });
+})
